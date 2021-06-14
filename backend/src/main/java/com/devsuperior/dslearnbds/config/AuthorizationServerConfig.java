5c83aa24514c26811d5335305072756a9b82c3e7
Cleber <webfaria@gmail.com>
2021-06-14 16:22:36 -0300

    Refresh token

--- backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -45,6 +46,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private JwtTokenEnhancer tokenEnhancer;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -57,9 +61,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient(clientId)
 			.secret(passwordEncoder.encode(clientSecret))
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
+			.authorizedGrantTypes("password", "refresh_token")
 			.accessTokenValiditySeconds(86400)
-			.accessTokenValiditySeconds(jwtDuration);
+			.accessTokenValiditySeconds(jwtDuration)
+			.refreshTokenValiditySeconds(jwtDuration);
 	}
 
 	@Override
@@ -70,6 +75,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints.authenticationManager(authenticationManager)
 				.tokenStore(tokenStore)
 				.accessTokenConverter(accessTokenConverter)
-				.tokenEnhancer(chain);
+				.tokenEnhancer(chain)
+				.userDetailsService(userDetailsService);
 	}
 }
